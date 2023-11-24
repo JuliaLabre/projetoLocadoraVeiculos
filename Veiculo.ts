386e1340ce818ef0ec6134c7a502ca9bba6c9513
Eduardo Assimo <61838816+eduardo-assimo@users.noreply.github.com>
2023-11-23 21:17:05 -0300

    criação da classe Veiculo.ts

---
+++ Veiculo.ts
@@ -0,0 +1,22 @@
+class Veiculo {
+    private id:number
+    private tipo:string  
+    private placa:string
+    private alugado:boolean
+
+    constructor(id:number, tipo:string, placa:string, alugado:boolean) {
+      this.id = id;
+      this.tipo = tipo;
+      this.placa = placa;
+      this.alugado = alugado;
+    }
+  }
+  
+  // Exemplo de uso da classe
+  const veiculo1 = new Veiculo(1,
+  "carro", "ABC1D23", false);
+  const veiculo2 = new Veiculo(2, "moto", "DEF4G56", false);
+  
+  console.log(veiculo1);
+  console.log(veiculo2);
+  
