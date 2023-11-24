5e0260a1ded3f662caad2937768c316da685781b
JuliaLabre <juliarlabres@gmail.com>
2023-11-24 14:59:50 -0300

    metodo locacao Silvio

--- class/Locacao.ts
+++ class/Locacao.ts
@@ -3,7 +3,7 @@ import { Veiculo } from "./Veiculo"
 
 export default class Locacao {
     private id:number
-    private cliente:Cliente  
+    private cliente:Cliente 
     private veiculo:Veiculo
     private periodoLocacao:number
 
@@ -13,4 +13,30 @@ export default class Locacao {
       this.veiculo = veiculo;
       this.periodoLocacao = periodoLocacao;
     }
-  }
+    calcularValorLocacao(valorDiaria: number): number {  
+    let acrescimoTipoVeiculo: number;
+    
+    if (this.veiculo.getTipo() === 'Moto') {
+      acrescimoTipoVeiculo = 0.05;
+    } else {
+      acrescimoTipoVeiculo = 0.1;
+    }
+
+    const valorAluguel = (valorDiaria * this.periodoLocacao) +  acrescimoTipoVeiculo;
+    return valorAluguel
+      
+    }
+
+    getIdLocacao(){
+      return this.id;
+    }
+    getCliente(){
+      return this.id;
+    }
+    getVeiculo(){
+      return this.veiculo;
+    }
+    getPeriodoLocacao(){
+      return this.periodoLocacao;
+    }
+    }
